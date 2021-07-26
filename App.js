5fbcb9dfe5f4102aa3cc31545d8191034a4f2fb2
Victor Leonardi dos Reis <victorleonardireis@poli.ufrj.br>
2021-07-25 23:55:20 -0300

    Primeiros estudos. Definição de cores e arrumando os desenhos.

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
+import { createDrawerNavigator } from 'react-navigation/drawer';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from 'react-navigation/native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style = {styles.menuBar}>
+        <View style ={styles.button}>
+
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +20,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#144357',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  menuBar:{
+    marginLeft: 291,
+    marginTop: 0,
+    width:69,
+    height:640,
+    backgroundColor: '#0F3140'
+  },
+  button:{
+
+  }
+
 });
